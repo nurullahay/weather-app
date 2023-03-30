5343f75a4d894ad7fd3f802fa4acf4e7b91957c2
nurullahay <nurullahay635@icloud.com>
2023-03-30 22:22:21 +0300

    deploy messagge

--- dist/_nuxt/static/1680201771/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FWeather","\u002Fdeneme"]})
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   target: 'static',
   router: {
-    base: '/<repository-name>/'
+    base: '/<weather-app>/'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
