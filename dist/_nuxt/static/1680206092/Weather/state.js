e7b9b94aa097ecdf7648e27c6de8636017696e88
nurullahay <nurullahay635@icloud.com>
2023-03-30 23:16:43 +0300

    github-pages-deploy

---
+++ dist/_nuxt/static/1680206092/Weather/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fweather-app\u002F_nuxt\u002Fstatic\u002F1680206092",layout:"default",error:a,serverRendered:true,routePath:"\u002FWeather",config:{_app:{basePath:"\u002Fweather-app\u002F",assetsPath:"\u002Fweather-app\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   target: 'static',
   router: {
-    base: '/<weather-app>/'
+    base: '/weather-app/'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
